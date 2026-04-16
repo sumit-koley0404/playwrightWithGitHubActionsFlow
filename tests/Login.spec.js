477e041dba719db3c3787f77cf6d267cf62795f5
sumit-koley0404 <sumitkoley180@gmail.com>
2026-04-16 13:18:41 +0530

    Fix:drive Username and Password from Github->settings->scerates and variables->actions->new repo secret

--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -1,9 +1,14 @@
-const{test,expect}=require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 
-test('Login test',async({page})=>{
+test('Login test', async ({ page }) => {
     await page.goto('https://rahulshettyacademy.com/client/');
-    await page.locator('#userEmail').fill("sumitkoley727@gmail.com");
-    await page.locator('#userPassword').fill("Sumit@123");
+    const username = process.env.USERNAME;
+    const password = process.env.PASSWORD;
+
+    await page.fill('#username', username);
+    await page.fill('#password', password);
+    // await page.locator('#userEmail').fill("sumitkoley727@gmail.com");
+    // await page.locator('#userPassword').fill("Sumit@123");
     await page.locator('#login').click();
     await expect(page).toHaveURL('https://rahulshettyacademy.com/client/#/dashboard/dash');
 })
