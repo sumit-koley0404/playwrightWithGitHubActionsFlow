ab31afc0eb3dde7a4bf2f63ca37faca7243b8337
sumit-koley0404 <sumitkoley180@gmail.com>
2026-04-16 13:29:06 +0530

    revert back

--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -2,13 +2,13 @@ const { test, expect } = require('@playwright/test');
 
 test('Login test', async ({ page }) => {
     await page.goto('https://rahulshettyacademy.com/client/');
-    const username = process.env.USERNAME;
-    const password = process.env.PASSWORD;
+    // const username = process.env.USERNAME;
+    // const password = process.env.PASSWORD;
 
-    await page.fill('#username', username);
-    await page.fill('#password', password);
-    // await page.locator('#userEmail').fill("sumitkoley727@gmail.com");
-    // await page.locator('#userPassword').fill("Sumit@123");
+    // await page.fill('#username', username);
+    // await page.fill('#password', password);
+    await page.locator('#userEmail').fill("sumitkoley727@gmail.com");
+    await page.locator('#userPassword').fill("Sumit@123");
     await page.locator('#login').click();
     await expect(page).toHaveURL('https://rahulshettyacademy.com/client/#/dashboard/dash');
 })
